4354dca65fa4a466478fabf3d7b8b2d4cf511cc8
Andres Gonzalez <jesterdm4@hotmail.com>
2023-02-10 08:22:21 -0400

    primer commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText('Learn React');
+  const linkElement = screen.getByText('Learn React or not');
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/Prueba.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Prueba = () => {
+  return (
+    <div>por favor, nota este cambio</div>
+  )
+}
+
+export default Prueba
